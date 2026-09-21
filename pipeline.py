cb2284ba824d0c0aa15ad93424e58dfbf490abeb
Richy <rxie0405@gmail.com>
2026-09-21 15:50:57 -0400

    added load_data into pipeline

--- pipeline.py
+++ pipeline.py
@@ -12,7 +12,7 @@ import argparse
 import logging
 import sys
 from pathlib import Path
-
+from data_loaders import load_data
 
 logger = logging.getLogger(__name__)
 
@@ -80,6 +80,11 @@ if __name__ == "__main__":
     setup_logging(verbose=args.verbose)
 
     if not validate_input(args.input):
+        sys.exit(1)
+
+    try:
+        data = load_data(args.input)
+    except ValueError:
         sys.exit(1)
 
     '''
